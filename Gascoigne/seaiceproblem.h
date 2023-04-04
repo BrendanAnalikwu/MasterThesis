06d02a5483046b16a08ba9b3bbaa0c9a70c5b185
BrendanAnalikwu <34724641+BrendanAnalikwu@users.noreply.github.com>
2023-04-04 10:34:37 +0200

    Added basics for SeaIceRHS

--- Gascoigne/seaiceproblem.h
+++ Gascoigne/seaiceproblem.h
@@ -9,7 +9,21 @@
 
 namespace Gascoigne {
 
-    class SeaIceEquation : public virtual Equation {
+    class SeaIceRHS : public DomainRightHandSide
+    {
+    public:
+        int GetNcomp() const override { return 2; }
+
+        std::string GetName() const override { return "Sea_Ice_Right_Hand_Side"; }
+
+        double operator()(int c, const Vertex2d& v) const override
+        {
+            return 0.; //TODO:  implement
+        }
+    };
+
+    class SeaIceEquation : public virtual Equation
+    {
     public:
         SeaIceEquation* createNew() const override { return new SeaIceEquation(); }
 
@@ -20,9 +34,9 @@ namespace Gascoigne {
         void point(double h, const Vertex2d &v) const {}
         void point(double h, const Vertex3d& v) const {}
 
-        void Form(VectorIterator b, const FemFunction& U, const TestFunction& N) const override { }
+        void Form(VectorIterator b, const FemFunction& U, const TestFunction& N) const override { } //TODO:  implement
 
-        void Matrix(EntryMatrix& A, const FemFunction& U, const TestFunction& M, const TestFunction &N) const override { }
+        void Matrix(EntryMatrix& A, const FemFunction& U, const TestFunction& M, const TestFunction &N) const override { } //TODO:  implement
     };
 
     class SeaIceProblem : public ProblemDescriptorBase {
@@ -30,7 +44,7 @@ namespace Gascoigne {
             GetParamFile() = pf;
 
             GetEquationPointer() = new SeaIceEquation;
-            GetRightHandSidePointer() = new ...;
+            GetRightHandSidePointer() = new SeaIceRHS;
             GetDirichletDataPointer() = new ...;
 
         }
