Fixes in the PINN loss computation

data_driven.py
@@ -19,11 +19,12 @@ G = 1.
 C_o = 1026 * 5.5e-3 * L / (900 * G)
 C_a = 1.3 * 1.2e-3 * L / (900 * G)
 C_r = 27.5e3 * T ** 2 / (2 * 900 * L ** 2)
-e_2 = .5
+e_2 = .25
 C = 20
 f_c = 1.46e-4
 dx = 512e3 / 256 / L  # 4km
-dt = 1e3 / T  # 1000s
+dT = 1e3 / T  # 1000s
+
 
 def read_velocities(filenames: List[str]):
     res = torch.zeros((len(filenames), 2, 257, 257))
@@ -69,7 +70,7 @@ v_a[:, 1, :, :] = (-sin(alpha) * (x - midpoint) + cos(alpha) * (y - midpoint)) *
 v_a = -v_a * 1e-1  # correct scaling should be *1e-3 and then *1e4
 v_a = v_a.to(dev)
 
-v_o = torch.empty(1, 2, 257, 257)
+v_o = torch.empty(1, 2, 257, 257, device=dev)
 v_o[:, 0] = .01 * (y/250 - 1) * 1e-3
 v_o[:, 1] = .01 * (1 - x/250) * 1e-3
 
@@ -77,6 +78,8 @@ criterion = torch.nn.MSELoss().to(dev)
 optim = torch.optim.Adam(model.parameters(), lr=1e-4)
 losses = []
 PINN_losses = []
+
+loss_func(label * 1e-4, H, A, data * 1e-4, v_a * 1e-2, v_o, C_r, C_a, C_o, T, e_2, C, f_c, dx, 1.)
 
 pbar = trange(10000)
 for i in pbar:

loss.py
@@ -249,17 +249,18 @@ def loss_func(dv: torch.Tensor, H: torch.Tensor, A: torch.Tensor, v_old: torch.T
     time_deriv = integration_B_grid(dv, H, dx) / dt
 
     v_c = torch.empty_like(dv)
-    v_c[:, 0] = -v_old[:, 1] - dv[:, 1]
-    v_c[:, 1] = v_old[:, 0] + dv[:, 0]
+    v_o_diff = v_o - v_old - dv
+    v_c[:, 0] = v_o_diff[:, 1]
+    v_c[:, 1] = -v_o_diff[:, 0]
     coriolis_term = integration_B_grid(T*f_c*v_c, H, dx)
 
-    v_o_diff = v_o - v_old - dv
-    t_o = C_o * torch.mul(torch.linalg.norm(v_o_diff, dim=1, keepdim=True), v_o_diff)
-    ocean_term = integration_B_grid(t_o, A, dx)
+    # v_o_diff = v_o - v_old - dv
+    t_o = C_o * torch.mul(torch.sqrt(v_o_diff.square().sum(1, keepdim=True) + 1e-8), v_o_diff)
+    ocean_term = integration_B_grid(t_o, torch.ones_like(A), dx)
 
     # Compute wind term in RHS (C_a A τ_a, ϕ)
     t_a = C_a * torch.mul(torch.linalg.norm(v_a, dim=1, keepdim=True), v_a)
-    wind_term = integration_B_grid(t_a, A, dx)
+    wind_term = integration_B_grid(t_a, torch.ones_like(A), dx)
 
     # Stress computation
     s_xx, s_yy, s_xy = internal_stress(v_old + dv, H, A, dx, C, e_2)
@@ -271,6 +272,6 @@ def loss_func(dv: torch.Tensor, H: torch.Tensor, A: torch.Tensor, v_old: torch.T
     # A - F
     fem_total = time_deriv + coriolis_term - stress_term - ocean_term - wind_term
 
-    return (dx ** -3 * torch.sum(torch.pow(fem_total, 2))
+    return (dx ** -2 * torch.sum(torch.pow(fem_total, 2))
             + torch.sum(torch.pow(dv[:, :, 0, :], 2)) + torch.sum(torch.pow(dv[:, :, -1, :], 2))
-            + torch.sum(torch.pow(dv[:, :, 0, 1:-1], 2)) + torch.sum(torch.pow(dv[:, :, -1, 1:-1], 2))) * 1e6
+            + torch.sum(torch.pow(dv[:, :, 0, 1:-1], 2)) + torch.sum(torch.pow(dv[:, :, -1, 1:-1], 2)))