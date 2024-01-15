fc27a689df8e65cc6427c7ef2139ab0a616c1dcf
BrendanAnalikwu <34724641+BrendanAnalikwu@users.noreply.github.com>
2024-01-15 09:53:47 +0100

    Added bash file to git

---
+++ train_model.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name="global_surrogate"
+#SBATCH --partition=gpu
+#SBATCH --gpus=1
+#SBATCH --mem=20G
+#SBATCH --time=400 # default in minutes
+#SBATCH --account=education-eemcs-msc-am
+#SBATCH --ntasks=1
+
+
+module load 2023r1 python openmpi cuda/11.7 py-pip py-tqdm
+python -m pip install --user vtk torch==2.0.1 torchvision
+
+srun python data_driven.py ../data_gen/Results/ 16 16 32 0 0 20000 1 $SLURM_ARRAY_TASK_ID
+
