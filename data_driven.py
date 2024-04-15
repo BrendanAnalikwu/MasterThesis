Minor changes incl: fix droplast, enabled scheduler, added test_dataloader, added option to save results every 5 epochs, added argparse, implemented BaseNorm, added tanh scaling for label, and other minor changes

data_driven.py
@@ -33,14 +33,15 @@ def getParameters(model: torch.nn.Module, lr: float = .1):
     return params
 
 
-def train(model, dataset, dev, n_steps=128, main_loss='MSE', job_id=None, betas=(.9, .999), batch_size=8):
-    test_dataset, train_dataset = dataset.get_test_train_split(.2)
-    dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=False)
+def train(model, dataset, dev, n_steps=128, main_loss='MSE', job_id=None, betas=(.9, .999), batch_size=8, save=True):
+    test_dataset, train_dataset = dataset.get_test_train_split(.2, job_id)
+    dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=len(train_dataset) > batch_size)
+    test_dataloader = DataLoader(test_dataset, batch_size=64, shuffle=False, drop_last=len(test_dataset) > 64)
 
     criterion = Loss(main_loss).to(dev)
     test_criterion = Loss(main_loss).to(dev)
-    optim = torch.optim.Adam(getParameters(model, 1e-2), lr=1e-3, betas=betas)
-    # scheduler = ReduceLROnPlateau(optim, patience=100, min_lr=1e-9)
+    optim = torch.optim.Adam(getParameters(model, 1e-3), lr=1e-3, betas=betas)
+    scheduler = ReduceLROnPlateau(optim, patience=10, min_lr=1e-9)
     last_lr = optim.param_groups[0]['lr']
 
     model_id = f"{model.__class__.__name__}_{main_loss}"
@@ -53,14 +54,15 @@ def train(model, dataset, dev, n_steps=128, main_loss='MSE', job_id=None, betas=
 
     reg_n = sum(p.numel() for p in model.parameters())
     regs = []
+    # params = [[p.abs().mean().cpu().detach() for p in model.parameters() if p.dim() > 3]]
 
-    pbar = trange(n_steps, mininterval=1.)
+    pbar = trange(n_steps)
     for i in pbar:
         for (data, H, A, v_a, label) in dataloader:
             # Forward pass and compute output
-            autoencoder_output = model.encoder(label, H, A, v_a)
+            # autoencoder_output = model.encoder(label, H, A, v_a)
             # encoding_output = model.encoder(data, H, A, v_a)
-            output = model.decoder(*autoencoder_output)
+            output = model(data, H, A, v_a)
 
             # Compute losses
             with torch.no_grad():
@@ -76,26 +78,30 @@ def train(model, dataset, dev, n_steps=128, main_loss='MSE', job_id=None, betas=
             optim.step()
             torch.cuda.empty_cache()
 
-            # scheduler.step(np.mean(criterion.results[main_loss][-len(dataloader):]))
-            if optim.param_groups[0]['lr'] != last_lr:
-                last_lr = optim.param_groups[0]['lr']
-                print(f"NEW LEARNING RATE: {last_lr}")
-                pbar.update()
+        scheduler.step(np.mean(criterion.results[main_loss][-len(dataloader):]))
+        if optim.param_groups[0]['lr'] != last_lr:
+            last_lr = optim.param_groups[0]['lr']
+            print(f"NEW LEARNING RATE: {last_lr}")
+            pbar.update()
 
+        # params.append([p.abs().mean().cpu().detach() for p in model.parameters() if p.dim() > 3])
         model.eval()
         with torch.no_grad():
-            t_data, t_H, t_A, t_v_a, t_label = test_dataset[:]
-            test_output = model(t_data, t_H, t_A, t_v_a)
-            test_criterion(test_output, t_label, t_data, t_A, store=True, grad=False)
+            for t_data, t_H, t_A, t_v_a, t_label in test_dataloader:
+                test_output = model(t_data, t_H, t_A, t_v_a)
+                test_criterion(test_output, t_label, t_data, t_A, store=False, grad=False)
+            test_criterion.flush_stack_to_results()
         model.train()
 
-        if i % 5 == 0:
+        if i % 5 == 0 and save:
             torch.save(model, f'model_{model_id}.pt')
             torch.save(criterion.results, f'losses_{model_id}.li')
             torch.save(test_criterion.results, f'test_losses_{model_id}.li')
 
         pbar.set_postfix(loss=np.mean(criterion.results[main_loss][-len(dataloader):]),
-                         test_loss=np.mean(test_criterion.results[main_loss][-len(dataloader):]))
+                         test_loss=test_criterion.results[main_loss][-1],
+                         mse=np.mean(criterion.results['MSE'][-len(dataloader):]),
+                         lr=last_lr)
 
     torch.save(model, f'model_{model_id}.pt')
     torch.save(criterion.results, f'losses_{model_id}.li')
@@ -105,17 +111,29 @@ def train(model, dataset, dev, n_steps=128, main_loss='MSE', job_id=None, betas=
 
 
 if __name__ == "__main__":
-    import sys
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', help='Specifies the path used to the data', type=str)
+    parser.add_argument('n_steps', help='Number of epochs', type=int)
+    parser.add_argument('main_loss', help='Name of the loss that is trained on', type=str, choices=Loss.loss_names)
+    parser.add_argument('model_name', help='Name of the model', type=str, choices=['SurrogateNet', 'UNet', 'Noisy', 'Small'])
+    parser.add_argument('-j', '--job_id', help='Slurm job id', type=str, default='0')
+    parser.add_argument('-b', '--betas', type=float, nargs=2, default=[.9, .999])
+    parser.add_argument('-B', '--batch_size', help='Batch size of mini-batches', type=int, default=16)
+    parser.add_argument('-p', '--physical', type=int, default=10, help='Time step after which physical states are assumed')
+    parser.add_argument('-s', '--nosave', help='Specifies if intermediate saving of losses and model is enables', action='store_false')
 
-    data_path = (sys.argv[1])
-    n_steps = int(sys.argv[2])
-    main_loss = str(sys.argv[3])
-    model_name = str(sys.argv[4])  # 'UNet' or else SurrogateNet
-    job_id = sys.argv[5]
-    betas = float(sys.argv[6]), float(sys.argv[7])
-    batch_size = 16
-    if len(sys.argv) == 9:
-        batch_size = int(sys.argv[8])
+    args = parser.parse_args()
+
+    data_path = args.path
+    n_steps = args.n_steps
+    main_loss = args.main_loss
+    model_name = args.model_name  # 'UNet' or else SurrogateNet
+    job_id = args.job_id
+    betas = args.betas
+    batch_size = args.batch_size
+    phys_i = args.physical
+    save = args.nosave
 
     dev = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
@@ -128,9 +146,9 @@ if __name__ == "__main__":
     else:
         model = SurrogateNet().to(dev)
 
-    dataset = FourierData(data_path, SeaIceTransform(), dev=dev, phys_i=25)
+    dataset = FourierData(data_path, SeaIceTransform(), dev=dev, phys_i=phys_i)
 
-    model, results = train(model, dataset, dev, n_steps, main_loss, job_id, betas, batch_size)
+    model, results = train(model, dataset, dev, n_steps, main_loss, job_id, betas, batch_size, save)
 
     # # Plot results
     # plot_comparison(model, dataset, 0, 0, patch_size, overlap)

dataset.py
@@ -3,7 +3,7 @@ from abc import ABC
 from collections import defaultdict
 from math import cos, sin, pi, sqrt, exp
 from random import randint, getrandbits
-from typing import List, Optional
+from typing import List, Optional, Union
 
 import numpy as np
 import torch
@@ -107,7 +107,21 @@ class SeaIceTransform(object):
             return x.rot90(rot, [-2, -1])
 
 
-class PixelNorm(object):
+class BaseNorm(object):
+    mean: Union[float, torch.Tensor]
+    std: Union[float, torch.Tensor]
+
+    def __call__(self, x: torch.Tensor):
+        return (x - self.mean) / self.std
+
+    def inverse(self, x: torch.Tensor):
+        return x * self.std + self.mean
+
+    def __repr__(self):
+        return f'{self.__class__.__name__} object with mean = {self.mean} and std = {self.std}'
+
+
+class PixelNorm(BaseNorm):
     def __init__(self, x: torch.Tensor, eps=1e-6, transforms=True, velocity=True):
         mean_ = x.mean(dim=0, keepdim=True)
         if velocity:
@@ -126,14 +140,8 @@ class PixelNorm(object):
                                   dim=0, keepdim=True).sqrt()
         self.std += eps
 
-    def __call__(self, x: torch.Tensor):
-        return (x - self.mean) / self.std
 
-    def inverse(self, x: torch.Tensor):
-        return x * self.std + self.mean
-
-
-class ChannelNorm(object):
+class ChannelNorm(BaseNorm):
     dims = (0, 2, 3)
 
     def __init__(self, x: torch.Tensor, eps=1e-6, transforms=True, velocity=True):
@@ -158,14 +166,8 @@ class ChannelNorm(object):
                  range(4)]).abs(), dim=self.dims, keepdim=True).sqrt()
         self.std += eps
 
-    def __call__(self, x: torch.Tensor):
-        return (x - self.mean) / self.std
 
-    def inverse(self, x: torch.Tensor):
-        return x * self.std + self.mean
-
-
-class InstanceNorm(object):
+class InstanceNorm(BaseNorm):
     dims = (2, 3)
 
     def __init__(self, x: torch.Tensor, eps=1e-6, transforms=True, velocity=True):
@@ -174,25 +176,13 @@ class InstanceNorm(object):
         self.std = torch.std(x, dim=self.dims, keepdim=True)
         self.std += eps
 
-    def __call__(self, x: torch.Tensor):
-        return (x - self.mean) / self.std
 
-    def inverse(self, x: torch.Tensor):
-        return x * self.std + self.mean
-
-
-class MinMaxNorm(object):
+class MinMaxNorm(BaseNorm):
     dims = (0, 2, 3)
 
     def __init__(self, x: torch.Tensor, eps=1e-6, transforms=True, velocity=True):
         self.mean = 0.
         self.std = x.abs().amax(dim=self.dims, keepdim=True) - self.mean
-
-    def __call__(self, x: torch.Tensor):
-        return (x - self.mean) / self.std
-
-    def inverse(self, x: torch.Tensor):
-        return x * self.std + self.mean
 
 
 class FourierData(SeaIceDataset):
@@ -201,16 +191,16 @@ class FourierData(SeaIceDataset):
     def __init__(self, basedir: str, transform: Optional[SeaIceTransform] = None, dev: torch.device = 'cpu',
                  phys_i: int = 0):
         self.transform = transform
-        dirs, fn_c = zip(*[(dn, os.path.join(dn, "coef.param")) for s in os.listdir(basedir) if
-                           os.path.isdir(subdir := os.path.join(basedir, s)) for d in
-                           os.listdir(subdir) if os.path.isdir(dn := os.path.join(subdir, d))])
+        dirs, fn_c, self.names = zip(*[(dn, os.path.join(dn, "coef.param"), d) for s in os.listdir(basedir) if
+                                       os.path.isdir(subdir := os.path.join(basedir, s)) for d in
+                                       os.listdir(subdir) if os.path.isdir(dn := os.path.join(subdir, d))])
         fn_v, is_label, is_data = zip(*[(os.path.join(d, fn), i != phys_i, i != (len(fl) - 1)) for d in dirs if
                                         (fl := list(filter(lambda n: n[0] == 'v', sorted(os.listdir(d))))) for i, fn
                                         in enumerate(fl) if i >= phys_i])
         self.t = [list(filter(lambda k: k > phys_i,
                               [int(fn.replace('v.', '').replace('.vtk', '')) for fn in os.listdir(d) if
                                fn[:2] == 'v.'])) for
-                  d in self.dirs]
+                  d in dirs]
 
         self.velocities = SeaIceDataset.scale_velocity(read_velocities(fn_v)).to(dev)
         self.data = self.velocities[list(is_data)]
@@ -247,7 +237,7 @@ class FourierData(SeaIceDataset):
                     coef[words[0]] = [float(x) for x in words[2:]]
             f.close()
 
-            ind = slice(j, j + len(t[i]))
+            ind = slice(j, j + len(self.t[i]))
             self.v_a[ind] = self.cyclone(coef, x, y, [self.dt * k for k in self.t[i]])
             j += len(self.t[i])
 
@@ -260,12 +250,14 @@ class FourierData(SeaIceDataset):
 
         # Perform scaling
         self.data = self.data_scaling(self.data)
-        self.label = self.label_scaling(self.label)
+        self.label = torch.tanh(self.label_scaling(self.label) * 10)
         self.v_a = self.v_a_scaling(self.v_a)
         self.H = self.H_scaling(self.H)
         self.A = self.A_scaling(self.A)
+        print(f'data scaling: {self.data_scaling}\nlabel scaling: {self.label_scaling}\n'
+              f'v_a scaling: {self.v_a_scaling}\nH scaling: {self.H_scaling}\nA scaling: {self.A_scaling}')
 
-    def get_test_train_split(self, frac: float = .2):
+    def get_test_train_split(self, frac: float = .2, output: Optional[str] = None):
         split_idx = int(len(self.t) * frac)
         idx = torch.randperm(len(self.t))
         test_idx = idx[:split_idx]
@@ -274,6 +266,9 @@ class FourierData(SeaIceDataset):
         indices: list[list[int]] = [list(range(m, m := (m+len(tx)))) for tx in self.t]
         test_indices = [i for idx in test_idx for i in indices[idx]]
         train_indices = [i for idx in train_idx for i in indices[idx]]
+        if output:
+            with open(f"test_data_{output}.txt", "w") as f:
+                f.write('\n'.join(str(self.names[i]) for i in test_idx))
         return torch.utils.data.Subset(self, test_indices), torch.utils.data.Subset(self, train_indices)
 
     @staticmethod

loss.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 from math import sqrt
 from typing import Tuple, Optional
 
+import numpy as np
 import torch
 import torch.utils.data
 
@@ -420,6 +421,7 @@ class Loss(torch.nn.Module):
         self.results = defaultdict(list)
         self.mre_eps = mre_eps
         self.mrde_eps = mrde_eps
+        self.stack = []
 
     def forward(self, dv: torch.Tensor, label: torch.Tensor, v_old: torch.Tensor, A: torch.tensor,
                 store: bool = True, grad: bool = True):
@@ -441,19 +443,14 @@ class Loss(torch.nn.Module):
         with torch.no_grad():
             mce = mean_concentration_loss(dv, label, v_old, A)
 
+        self.stack.append({'MAE': mae.item(), 'MSE': mse.item(), 'SRE': sre.item(),
+                           'MSE+SRE': msesre.item(), 'MSE+MRE': msemre.item(), 'MSE+MRDE': msemrde.item(),
+                           'MSE+SRE+MRDE': msesremrde.item(),
+                           'MRE': mre.item(), 'MRDE': mrde.item(),
+                           'MCE': mce.item()})
+
         if store:
-            self.results['MAE'].append(mae.item())
-            self.results['MSE'].append(mse.item())
-            self.results['SRE'].append(sre.item())
-            self.results['MSE+SRE'].append(msesre.item())
-            self.results['MSE+MRE'].append(msemre.item())
-            self.results['MSE+MRDE'].append(msemrde.item())
-            self.results['MSE+SRE+MRDE'].append(msesremrde.item())
-
-            self.results['MRE'].append(mre.item())
-            self.results['MRDE'].append(mrde.item())
-
-            self.results['MCE'].append(mce.item())
+            self.flush_stack_to_results()
 
         if self.main == 'MAE':
             return mae
@@ -471,6 +468,11 @@ class Loss(torch.nn.Module):
             return msesremrde
         elif self.main == 'MCE':
             return mce
+
+    def flush_stack_to_results(self):
+        for key in self.stack[0].keys():
+            self.results[key].append(np.mean([d[key] for d in self.stack]))
+        self.stack = []
 
     def validate(self, dv: torch.Tensor, label: torch.Tensor, v_old: torch.Tensor, A: torch.tensor, store: bool = True):
         with torch.no_grad():