664c31bd7e1c08a7debc149fc12ebee6ddd1aef0
BrendanAnalikwu <34724641+BrendanAnalikwu@users.noreply.github.com>
2024-06-18 15:02:17 +0200

    Added bayesian_optimization.py

---
+++ bayesian_optimization.py
@@ -0,0 +1,18 @@
+import numpy as np
+from sklearn.gaussian_process import GaussianProcessRegressor
+
+
+def get_scores():
+    pass
+
+
+def save_params():
+    pass
+
+
+if __name__ == "__main":
+    pass
+    # Load scores from register
+    # Train GP model
+    # Find next parameter set
+    # Save parameters to register
